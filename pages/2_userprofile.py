8b0d10d3b072948f56536d382102310475b4b42c
Iain <iainlim33cc@gmail.com>
2024-05-26 05:56:42 +0800

    changes

--- pages/2_userprofile.py
+++ pages/2_userprofile.py
@@ -7,22 +7,6 @@ import sqlite3
 conn = sqlite3.connect('volunteer.db', check_same_thread=False)
 c = conn.cursor()
 
-# Function to insert a new user profile into the database
-def insert_user_profile(user):
-    with conn:
-        c.execute(
-            "INSERT INTO users (username, date_of_birth, residential_area, occupation, volunteer_interests, skills) VALUES (?, ?, ?, ?, ?, ?)",
-            (user.username, user.date_of_birth, user.residential_area, user.occupation, user.volunteer_interests, user.skills)
-        )
-        conn.commit()
-
-# Function to get user profile from database
-def get_user_profile(username):
-    c.execute("SELECT * FROM users WHERE username = ?", (username,))
-    data = c.fetchone()
-    if data:
-        return UserProfile(*data[1:])  # Unpack all fields except the ID
-    return None
 
 # Define a list of volunteer interests
 volunteer_interests_options = [
