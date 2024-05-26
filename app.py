661952aff61d45007424631fc0b5ad7a41832350
16fb <toomuchdarius@gmail.com>
2024-05-26 10:51:16 +0800

    Added to links buttons

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 
+st.link_button("Dashboard", url="/dashboard")
 st.link_button("Create account", url="/userprofile")
+st.link_button("Find Near Me", url="/findNearMe")
+st.link_button("Organsiation new event form", url="https://forms.gle/QDyUpjkUr2cJGdpQA")
